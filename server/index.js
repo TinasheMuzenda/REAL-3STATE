580bd1b48df40e8b34f38320fc04f57f6098aaf3
TinasheMuzenda <tmuzendat@gmail.com>
2024-11-04 11:03:01 +0200

    server running and connected to DataBase

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 const app = express();
-app.listen(7000, () =>{
-    console.log('server is running on port 7000')
-})
+app.listen(7000, () => {
+  console.log("server is running on port 7000");
+});
