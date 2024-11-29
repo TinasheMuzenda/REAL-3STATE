096e6d437094684bfd1879e4e89c42364fa3b642
TinasheMuzenda <tmuzendat@gmail.com>
2024-11-29 07:25:48 +0200

    tried image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,43 @@
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 
 const Profile = () => {
   const currentUser = useSelector((state) => state.user);
+  const fileRef = useRef(null);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
-        <input type="text" placeholder="username" id="username" className="border p-3 rounded-lg"/>
-        <input type="text" placeholder="email" id="email" className="border p-3 rounded-lg"/>
-        <input type="text" placeholder="password" id="password" className="border p-3 rounded-lg"/>
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80">Update</button>
+        <input type="file" ref={fileRef} hidden accept="image/*" />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={currentUser}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80">
+          Update
+        </button>
       </form>
-      <div className="flex justify-between mt-5">
+      <div className="flex justify-between mt-5 ">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
